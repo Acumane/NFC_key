8b7efc264aac4c45c6c4d5f7b345b970162c9cf8
StefPres <stefan.prestrelski@gmail.com>
2024-03-07 18:54:28 -0800

    test with LCD screen. awaiting NFC reader.

--- hardware/NFC_Reader/NFC_Reader.ino
+++ hardware/NFC_Reader/NFC_Reader.ino
@@ -1,9 +1,17 @@
+#include <LiquidCrystal_I2C.h>
 #include <Adafruit_PN532.h>
 #include <PN532.h>
 
-void setup() {
-  // put your setup code here, to run once:
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 20 column and 4 rows
 
+void setup() {
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);            // move cursor the first row
+  lcd.print("Hello, Bren.");          // print message at the first row
+  lcd.setCursor(0, 1);            // move cursor to the second row
+  lcd.print("How are you?"); // print message at the second row
 }
 
 void loop() {
