99b03355d691cacfd1253d22390850bf69a84bfe
nawoda2 <nawoda3@gmail.com>
2024-03-21 19:09:42 -0700

    Added flutter_nfc_kit example

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,55 @@
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:nfc_manager/nfc_manager.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
-  ValueNotifier<dynamic> result = ValueNotifier(null);
+class _MyAppState extends State<MyApp> {
+  String _nfcData = 'NFC tag data will appear here';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('NFC Key')),
-        body: SafeArea(
-          child: FutureBuilder<bool>(
-            future: NfcManager.instance.isAvailable(),
-            builder: (context, ss) => ss.data != true
-                ? Center(child: Text('NfcManager.isAvailable(): ${ss.data}'))
-                : Flex(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    direction: Axis.vertical,
-                    children: [
-                      Flexible(
-                        flex: 2,
-                        child: Container(
-                          margin: EdgeInsets.all(4),
-                          constraints: BoxConstraints.expand(),
-                          decoration: BoxDecoration(border: Border.all()),
-                          child: SingleChildScrollView(
-                            child: ValueListenableBuilder<dynamic>(
-                              valueListenable: result,
-                              builder: (context, value, _) =>
-                                  Text('${value ?? ''}'),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FloatingActionButton(
-                            onPressed: _tagRead,
-                            child: Icon(Icons.barcode_reader),
-                          ),
-                          SizedBox(height: 20), // Add space between the buttons
-                          FloatingActionButton(
-                            onPressed: _ndefWrite,
-                            child: Icon(Icons.file_upload),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+        appBar: AppBar(
+          title: Text('Flutter NFC Kit Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_nfcData),
+              ElevatedButton(
+                child: Text('Read NFC'),
+                onPressed: () async {
+                  var availability = await FlutterNfcKit.nfcAvailability;
+                  if (availability == NFCAvailability.available) {
+                    try {
+                      var tag = await FlutterNfcKit.poll(timeout: Duration(seconds: 10),
+                          iosMultipleTagMessage: "More than one tag is found", iosAlertMessage: "Hold your device near the tag");
+                      setState(() {
+                        _nfcData = tag.toString();
+                      });
+                    } catch (e) {
+                      setState(() {
+                        _nfcData = 'Failed to read NFC: $e';
+                      });
+                    }
+                  } else {
+                    setState(() {
+                      _nfcData = 'NFC not available';
+                    });
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
     );
-  }
-
-  void _tagRead() {
-    result.value = 'Reading tag...';
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      result.value = tag.data;
-      NfcManager.instance.stopSession();
-    });
-  }
-
-  void _ndefWrite() {
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null || !ndef.isWritable) {
-        result.value = 'Tag is not ndef writable';
-        NfcManager.instance.stopSession(errorMessage: result.value);
-        return;
-      }
-
-      NdefMessage message = NdefMessage([
-        NdefRecord.createText('Hello World!'),
-        NdefRecord.createUri(Uri.parse('https://flutter.dev')),
-        NdefRecord.createMime(
-            'text/plain', Uint8List.fromList('Hello'.codeUnits)),
-        NdefRecord.createExternal(
-            'com.example', 'mytype', Uint8List.fromList('mydata'.codeUnits)),
-      ]);
-
-      try {
-        await ndef.write(message);
-        result.value = 'Success to "Ndef Write"';
-        NfcManager.instance.stopSession();
-      } catch (e) {
-        result.value = e;
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
-    });
   }
 }
