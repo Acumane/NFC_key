a4b4a41832bda4fcc44f7bf5b7cb4f95226b2772
StefPres <stefan.prestrelski@gmail.com>
2024-04-24 19:40:54 -0700

    One more commit just to sync things

--- hardware/i2c_NDEF/i2c_NDEF.ino
+++ hardware/i2c_NDEF/i2c_NDEF.ino
@@ -6,6 +6,8 @@ PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 String tagId = "None";
 byte nuidPICC[4];
+
+int tagnum = 0;
  
 void setup(void) 
 {
@@ -23,10 +25,11 @@ void readNFC()
 {
  if (nfc.tagPresent())
  {
+   tagnum++;
    NfcTag tag = nfc.read();
    tag.print();
    tagId = tag.getUidString();
-   Serial.println(" ");
+   Serial.println("tagnum " + String(tagnum));
  }
  delay(1000);
 }
